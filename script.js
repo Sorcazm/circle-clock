32af5f3e6b1713d06b0a3b57c26891fd2816b6a4
Sorcazm <erikamaslyan@gmail.com>
2022-11-29 08:51:38 +0200

    finish

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const minutesHand = document.getElementById('minute')
 const hoursHand = document.getElementById('hour')
+const secondsHand = document.getElementById('second')
+
 
 function getTime() {
-	const now = new Date()
-	const minutes = now.getMinutes()
-	const hours = now.getHours()
-	const timeInterval = 6
+    const now = new Date()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const seconds = now.getSeconds()
+    const timeInterval = 6
 
-	console.log(minutes,hours)
-	minutesHand.style.transform = 'rotate(' + (minutes * timeInterval) +'deg)'
-	hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes/2) +'deg)'
+    console.log(minutes, hours, seconds)
+    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)'
+    minutesHand.style.transform = 'rotate(' + (minutes * timeInterval) + 'deg)'
+    hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2) + 'deg)'
 }
 
 setInterval(getTime, 1000)
